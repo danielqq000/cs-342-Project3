8527f19e62a91c3ac861a22f4615a57d140e1f1c
jthom-54 <jthom54@uic.edu>
2023-04-16 16:24:59 -0500

    ver1.2

---
+++ Client/src/main/java/Deck.java
@@ -0,0 +1,34 @@
+import javafx.scene.image.Image;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Deck {
+    private String cardSR;
+    private Map<String, Card> deck = new HashMap<String, Card>();
+    //1, 2, 3, 4 : clubs, spades, hearts, diamonds
+    //11, 12, 13 : ace, queen, king
+    public void cardSort() {
+        String image;
+        int value = 0;
+        for (int suit = 1; suit <= 4; suit++) {
+            for (int rank = 1; rank <= 13; rank++) {
+                if (rank == 10 || rank == 23 || rank == 36 || rank == 49)
+                    continue;
+                if (rank%13 == 0)
+                    value = 11;
+                else
+                    value = rank%13+1;
+                image = rank + ".png";
+                Image cardFace = new Image(image, 92, 95, true, true);
+                Card card = new Card(value, suit, cardFace);
+                cardSR = String.valueOf(suit) + "," + String.valueOf(value);
+                deck.put(cardSR, card);
+            }
+        }
+    }
+
+    public Card getCard(String cardSR) {
+        return deck.get(cardSR);
+    }
+}
